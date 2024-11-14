54c067fbf880fad6bdf92a26573e9b08ca67e464
Sergio García Santamarina <UO294636@uniovi.es>
2024-11-14 09:09:41 +0100

    Add files via upload

---
+++ F1 DESKTOP/js/fondo.js
@@ -0,0 +1,22 @@
+class Fondo{
+    constructor(countryName,capitalName,nameOfTheCircuit){
+        this.countryName = countryName;
+        this.capitalName = capitalName;
+        this.nameOfTheCircuit = nameOfTheCircuit;
+    }
+
+    imagenCircuito(){
+    const varApiFlicker = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
+    $.getJSON(varApiFlicker,{
+        tags: this.nameOfTheCircuit,
+        tagmode: "any",
+        format: "json"
+    }).done(function(data){
+        let indiceFoto =Math.floor(Math.random() * data.items.length);
+        let foto = data.items[indiceFoto];
+        let fotoURL = foto.media.m;
+        $('body').css('background-image','url('+fotoURL.replace('_m','_b')+')').css('background-size', 'cover');
+    }
+    );
+    }
+}
